d76b1499dfead4cd6ad1c6296ebc5b6471275218
Benedito Magalhães <bencaceres1799@gmail.com>
2023-06-18 13:23:05 -0300

    Inserido alterações do index e informações de autenticação

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,5 +2,8 @@
 
 @section('conteudo')
   <h1>Sistema Escolar</h1>
-  <p>Sistem Web para cadastro escolar em Laravel</p>
+  <h2>Sistem Web para cadastro escolar em Laravel</ph2>
+  <h2>Feito por:</h2>
+  <h3><a  style="color:black; text-decoration: none" href="https://github.com/BeneditoDev1" target="_blank" rel="noopener noreferrer">Benedito Magalhães</a></h3>
+  <h3>Edmilson Silva</h3>
 @endsection
